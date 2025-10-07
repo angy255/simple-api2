28072481c756563afea64974e957e6200faa892e
Angelica Matos <angelicamatos255@gmail.com>
2025-10-07 16:50:48 -0400

    add css styling

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 document.querySelector("button").addEventListener("click", getHolidayInfo);
 
 function getHolidayInfo() {
@@ -7,7 +5,6 @@ function getHolidayInfo() {
   const countryISO = document.getElementById("country").value;
   const year = document.getElementById("year").value;
   const month = document.getElementById("month").value;
-
 
   //calendarific api website https://calendarific.com/api-documentation
   // include country, year and month
@@ -17,22 +14,23 @@ function getHolidayInfo() {
   console.log(countryISO);
 
   fetch(url)
-    .then((res) => 
-      res.json())
+    .then((res) => res.json())
 
     .then((data) => {
-      console.log(
-        "calendarific sends info", data
-      );
+      console.log("calendarific sends info", data);
 
-      console.log('show me the data', data);
+      console.log("show me the data", data);
       // maybe show data.response.holidays[0].date.iso
-      document.querySelector("h2").innerText = data.response.holidays[0].description;
-      document.querySelector("p").innerText = new Date (data.response.holidays[0].date.iso).toLocaleDateString();
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString 
-      document.querySelector("h4").innerText = data.response.holidays[0].primary_type;
+      document.querySelector("h2").innerText =
+        data.response.holidays[0].description;
+      document.querySelector("p").innerText = new Date(
+        data.response.holidays[0].date.iso
+      ).toLocaleDateString();
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleTimeString
+      document.querySelector("h4").innerText =
+        data.response.holidays[0].primary_type;
     })
     .catch((err) => {
       console.error("error", err);
     });
-  }
+}
